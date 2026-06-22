ac9af77910aaca7de064e640b7511a3411df8206
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-22 21:03:36 +0100

    feat: implement BoardManager.cs

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Tilemap m_Tilemap;
+
+    public int Width;
+    public int Height;
+    public Tile[] GroundTiles;
+
     void Start()
     {
-        
-    }
+        m_Tilemap = GetComponentInChildren<Tilemap>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                int tileNumber = Random.Range(0, GroundTiles.Length);
+                m_Tilemap.SetTile(new Vector3Int(x, y, 0), GroundTiles[tileNumber]);
+            }
+        }
     }
 }
