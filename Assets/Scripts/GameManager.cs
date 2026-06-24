bc8f327efcb6bf91bce206eb9a9b0ec9cc94c2a2
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-24 17:45:08 +0100

    feat: implement Start of GameManager.cs, update BoardManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private TurnManager m_TurnManager;
+
+    public BoardManager BoardManager;
+    public PlayerController PlayerController;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        m_TurnManager = new TurnManager();
+        BoardManager.Init();
+        PlayerController.Spawn(BoardManager, new Vector2Int(1, 1));
+
     }
 
     // Update is called once per frame
