7eb6e2ac61039ebab8bf22a01edbdf848826a5a1
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-24 17:48:04 +0100

    feat: implement TurnManager.cs

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class TurnManager
 {
-    
+    private int m_TurnCount;
+
+    public TurnManager()
+    {
+        m_TurnCount = 1;
+    }
+
+    public void Tick()
+    {
+        m_TurnCount++;
+    }
 }
