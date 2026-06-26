108c392d9c1c76908ada71988b1ed9fa8cc77019
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-26 09:11:17 +0100

    feat: override PlayerEntered in FoodObject.cs

--- Assets/Scripts/FoodObject.cs
+++ Assets/Scripts/FoodObject.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
-public class FoodObject : MonoBehaviour
+public class FoodObject : CellObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public override void PlayerEntered()
     {
-        
-    }
+        Destroy(gameObject);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Food increased");
     }
 }
