575fc299f1bb4efe48acd506b6302a2726e5a082
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-26 08:58:27 +0100

    feat: add method to BoardManager.cs to generate food in different cells of the board

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -5,24 +6,28 @@ public class BoardManager : MonoBehaviour
 {
     public class CellData
     {
-        public bool Passable;    
+        public bool Passable;
+        public GameObject ContainedObject;    
     }
 
     private Tilemap m_Tilemap;
     private CellData[,] m_BoardData;
     private Grid m_Grid;
+    private List<Vector2Int> m_EmptyCells;
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
     public PlayerController Player;
+    public GameObject FoodPrefab;
 
     public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
         m_BoardData = new CellData[Width, Height];
+        m_EmptyCells = new List<Vector2Int>();
 
         for (int y = 0; y < Height; y++)
         {
@@ -35,17 +40,23 @@ public class BoardManager : MonoBehaviour
                 if (isBorder)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    
                     m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+
                     m_BoardData[x, y].Passable = true;
+                    m_EmptyCells.Add(new Vector2Int(x, y));
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+
+        m_EmptyCells.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 
     public Vector3 CellToWorld(Vector2Int cellIndex)
@@ -61,5 +72,21 @@ public class BoardManager : MonoBehaviour
         }
 
         return m_BoardData[cellIndex.x, cellIndex.y];
+    }
+
+    void GenerateFood()
+    {
+        int foodCount = 5;
+        for (int i = 0; i < foodCount; i++)
+        {
+            int randomIndex = Random.Range(0, m_EmptyCells.Count);
+            Vector2Int coord = m_EmptyCells[randomIndex];
+
+            m_EmptyCells.RemoveAt(randomIndex);
+            CellData data = m_BoardData[coord.x, coord.y];
+            GameObject newFood = Instantiate(FoodPrefab);
+            newFood.transform.position = CellToWorld(coord);
+            data.ContainedObject = newFood;
+        }
     }
 }
