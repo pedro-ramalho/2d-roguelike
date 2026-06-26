c7726a46ff885828100a633a60ca609f49538bdb
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-26 09:25:33 +0100

    feat: updated FoodObject.cs so that different prefabs can assign different values of food points given

--- Assets/Scripts/FoodObject.cs
+++ Assets/Scripts/FoodObject.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class FoodObject : CellObject
 {
-    public int Points { get; private set; } = 5;
+    public int Points = 5;
     public override void PlayerEntered()
     {
         Destroy(gameObject);
