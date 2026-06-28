d87762e6ba6517d56f943eb851f4b0c6844d22b2
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-28 14:00:29 +0100

    feat: update GameManager.cs and ExitCellObject.cs to generate a new level once the Player hits the exit cell

--- Assets/Scripts/ExitCellObject.cs
+++ Assets/Scripts/ExitCellObject.cs
@@ -12,5 +12,5 @@ public class ExitCellObject : CellObject
         GameManager.Instance?.BoardManager?.SetCellTile(coord, endTile);
     }
 
-    public override void PlayerEntered() => Debug.Log("Player entered exit cell");
+    public override void PlayerEntered() => GameManager.Instance?.NewLevel();
 }
