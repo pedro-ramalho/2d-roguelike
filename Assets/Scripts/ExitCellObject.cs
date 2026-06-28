5e425a2ba8ef5fad9e6952d6e65a1028a806d0af
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-28 13:52:25 +0100

    feat: basic implementation of ExitCellObject.cs

--- Assets/Scripts/ExitCellObject.cs
+++ Assets/Scripts/ExitCellObject.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ExitCellObject : CellObject
 {
+    public Tile endTile;
 
+    public override void Init(Vector2Int coord)
+    {
+        base.Init(coord);
+
+        GameManager.Instance?.BoardManager?.SetCellTile(coord, endTile);
+    }
+
+    public override void PlayerEntered() => Debug.Log("Player entered exit cell");
 }
