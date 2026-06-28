ccc5d821b543ae8e79b977627af70812b810d965
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-28 15:05:05 +0100

    feat: add a Walk animation clip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float MoveSpeed = 3f;
+
     private BoardManager m_Board;
     private Vector2Int m_CellPosition;
     private bool m_IsGameOver;
+    private Animator m_Animator;
+    private bool m_IsMoving;
+    private Vector3 m_MoveTarget;
+
+    private void Awake() => m_Animator = GetComponent<Animator>();
 
     public void Init() => m_IsGameOver = false;
 
@@ -46,6 +53,26 @@ public class PlayerController : MonoBehaviour
             hasMoved = true;
         }
 
+        if (m_IsMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_MoveTarget, MoveSpeed * Time.deltaTime);
+
+            if (transform.position == m_MoveTarget)
+            {
+                m_IsMoving = false;
+                m_Animator.SetBool("Moving", false);
+
+                var cellData = m_Board.GetCellData(m_CellPosition);
+
+                if (cellData.ContainedObject != null)
+                {
+                    cellData.ContainedObject.PlayerEntered();
+                }
+            }
+
+            return;
+        }
+
         if (hasMoved)
         {
             BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
@@ -70,12 +97,24 @@ public class PlayerController : MonoBehaviour
     public void Spawn(BoardManager boardManager, Vector2Int cell)
     {
         m_Board = boardManager;
-        MoveTo(cell);
+        MoveTo(cell, true);
     }
 
-    public void MoveTo(Vector2Int cell)
+    public void MoveTo(Vector2Int cell, bool immediate = false)
     {
         m_CellPosition = cell;
-        transform.position = m_Board.CellToWorld(m_CellPosition);
+
+        if (immediate)
+        {
+            m_IsMoving = false;
+            transform.position = m_Board.CellToWorld(m_CellPosition);    
+        }
+        else
+        {
+            m_IsMoving = true;
+            m_MoveTarget = m_Board.CellToWorld(m_CellPosition);
+        }
+
+        m_Animator.SetBool("Moving", m_IsMoving);
     }
 }
