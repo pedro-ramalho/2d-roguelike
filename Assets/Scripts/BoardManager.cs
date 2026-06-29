38920d7c80467c5a244dda4d26968fc1a8fc9ec6
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-29 20:14:25 +0100

    feat: add GenerateEnemy method in BoardManager.cs

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -66,6 +66,7 @@ public class BoardManager : MonoBehaviour
 
         GenerateWall();
         GenerateFood();
+        GenerateEnemy();
     }
 
     public Vector3 CellToWorld(Vector2Int cellIndex)
@@ -156,5 +157,16 @@ public class BoardManager : MonoBehaviour
             
             AddObject(newWall, coord);  
         }
+    }
+
+    void GenerateEnemy()
+    {
+        int randomIndex = Random.Range(0, m_EmptyCells.Count);
+        Vector2Int coord = m_EmptyCells[randomIndex];
+
+        m_EmptyCells.RemoveAt(randomIndex);
+        EnemyObject newEnemy = Instantiate(EnemyPrefab);
+
+        AddObject(newEnemy, coord);
     }
 }
