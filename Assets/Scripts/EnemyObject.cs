d3967911fd7ca03f0f519ee088359378b819301b
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-06-29 20:22:22 +0100

    feat: update EnemyObject.cs, override Init and PlayerWantsToEnter methods, add health private fields

--- Assets/Scripts/EnemyObject.cs
+++ Assets/Scripts/EnemyObject.cs
@@ -1,8 +1,35 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EnemyObject : CellObject
 {
+    private int m_HealthPoint;
+
+    public int MaxHealth = 3;
     public int FoodDamage = 5;
+
+
+    public override void Init(Vector2Int cell)
+    {
+        base.Init(cell);
+
+        m_HealthPoint = MaxHealth;
+    }
+
+    public override bool PlayerWantsToEnter()
+    {
+        m_HealthPoint--;
+
+        // Enemy is still not dead, so Player cannot enter the cell yet
+        if (m_HealthPoint > 0)
+        {
+            return false;
+        }
+
+        Destroy(gameObject);
+
+        return true;
+    }
 
     public override void PlayerEntered() => GameManager.Instance?.ChangeFood(-FoodDamage);
 }
