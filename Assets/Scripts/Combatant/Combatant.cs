04817837a9fbd485fe8ab488ce0b8f7cbbab062d
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-06 21:19:30 +0100

    feat: updated the implementation of Combatant.cs to satisfy AC
    - Created an empty StatusEffect.cs script
    - Added a list of status effects to Combatant.cs
    - Added four methods to Combatant.cs: Heal, TakeDamage, AddBlock, AddStatusEffect

--- Assets/Scripts/Combatant/Combatant.cs
+++ Assets/Scripts/Combatant/Combatant.cs
@@ -1,20 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Combatant : MonoBehaviour
 {
     [SerializeField]
     private int m_MaxHP = 10;
+    private List<StatusEffect> m_StatusEffects;
 
-    public int HP { get; private set; }
-    public int Block { get; private set; }
+    public int MaxHP => m_MaxHP;
+    public int CurrentHP { get; private set; }
+    public int CurrentBlock { get; private set; }
+    public IReadOnlyList<StatusEffect> StatusEffects => m_StatusEffects;
 
-    void Awake()
+    protected virtual void Awake()
     {
-        HP = m_MaxHP;
-        Block = 0;
+        CurrentHP = m_MaxHP;
+        CurrentBlock = 0;
+        m_StatusEffects = new List<StatusEffect>();
     }
 
-    public void ChangeHealth(int amount) =>HP = Mathf.Clamp(HP + amount, 0, m_MaxHP);
+    public void Heal(int amount) => CurrentHP = Mathf.Clamp(CurrentHP + amount, 0, m_MaxHP);
+    
+    public void TakeDamage(int amount)
+    {
+        int damageAfterBlock = Mathf.Max(amount - CurrentBlock, 0);
+        CurrentBlock = Mathf.Max(CurrentBlock - amount, 0);
+        CurrentHP = Mathf.Max(CurrentHP - damageAfterBlock, 0);
+    }
 
-    public void ChangeBlock(int amount) => Block = Mathf.Max(Block + amount, 0);
+    public void AddBlock(int amount) => CurrentBlock = Mathf.Max(CurrentBlock + amount, 0);
+
+    public void AddStatusEffect(StatusEffect effect) => m_StatusEffects.Add(effect);
 }
