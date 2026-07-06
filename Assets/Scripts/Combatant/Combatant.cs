1c27e78a2db53438ef1574ef6215e5e310bf7b51
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-06 20:51:50 +0100

    feat: added stat fields to Combatant.cs and public methods to change health and block

--- Assets/Scripts/Combatant/Combatant.cs
+++ Assets/Scripts/Combatant/Combatant.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
-public class Combatant
+public abstract class Combatant : MonoBehaviour
 {
-    
+    [SerializeField]
+    private int m_MaxHP = 10;
+
+    public int HP { get; private set; }
+    public int Block { get; private set; }
+
+    void Awake()
+    {
+        HP = m_MaxHP;
+        Block = 0;
+    }
+
+    public void ChangeHealth(int amount) =>HP = Mathf.Clamp(HP + amount, 0, m_MaxHP);
+
+    public void ChangeBlock(int amount) => Block = Mathf.Max(Block + amount, 0);
 }
