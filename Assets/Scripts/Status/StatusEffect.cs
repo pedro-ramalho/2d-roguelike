d5cb0442aaa36c9875812ab62922acb8bb8e5411
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-07 13:28:05 +0100

    feat: updated StatusEffect.cs, add virtual methods for modifying damage and decreasing duration on turn end

--- Assets/Scripts/Status/StatusEffect.cs
+++ Assets/Scripts/Status/StatusEffect.cs
@@ -13,8 +13,13 @@ public abstract class StatusEffect
     public int Duration { get; set; }
     public StatusEffectType Type { get; }
 
-    protected StatusEffect(StatusEffectType type)
+    protected StatusEffect(StatusEffectType type, int duration)
     {
         Type = type;
+        Duration = duration;
     }
+
+    public virtual float ModifyOutgoingDamage(float damage) => damage;
+    public virtual float ModifyIncomingDamage(float damage) => damage;
+    public virtual void OnTurnEnd(ICombatant combatant) => Duration--;
 }
