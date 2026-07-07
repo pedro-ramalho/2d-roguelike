1ca300c9263651849987b12f7ce2b9e90a2f2c60
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-07 13:37:04 +0100

    feat: updated CombatantDamage.cs to iterate through the list of status effects of the attacker and defender

--- Assets/Scripts/Combatant/CombatantDamage.cs
+++ Assets/Scripts/Combatant/CombatantDamage.cs
@@ -4,6 +4,14 @@ public static class CombatantDamage
 {
     public static DamageResult ApplyDamage(ICombatant attacker, ICombatant defender)
     {
-        return defender.TakeDamage(attacker.Attack);
+        float damage = attacker.Attack;
+
+        foreach (StatusEffect status in attacker.StatusEffects)
+            damage = status.ModifyOutgoingDamage(damage);
+
+        foreach (StatusEffect status in defender.StatusEffects)
+            damage = status.ModifyIncomingDamage(damage);
+
+        return defender.TakeDamage(Mathf.FloorToInt(damage));
     }
 }
