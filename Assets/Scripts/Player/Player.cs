c15223ff63e9b2ada4473347b69d006b573f2243
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-08 17:35:50 +0100

    feat: add stamina related fields to Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 public class Player : MonoBehaviour, ICombatant
 {
     [SerializeField] private int m_MaxHP = 100;
+    [SerializeField] private int m_MaxStamina = 20;
     [SerializeField] private int m_Attack = 10;
+    private int m_Stamina;
     private CombatantState m_State;
 
     public int Attack => m_State.Attack;
     public int MaxHP => m_MaxHP;
     public int HP => m_State.HP;
+    public int MaxStamina => m_MaxStamina;
+    public int Stamina => m_Stamina;
     public int Block => m_State.Block;
     public IReadOnlyList<StatusEffect> StatusEffects => m_State.StatusEffects;
 
@@ -17,6 +21,8 @@ public class Player : MonoBehaviour, ICombatant
     {
         m_State = new CombatantState(m_MaxHP, m_Attack);
         m_State.AddBlock(7);
+
+        m_Stamina = m_MaxStamina;
     }
 
     public DamageResult TakeDamage(int amount) => m_State.TakeDamage(amount);
