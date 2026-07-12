3b91c7dd0fe106c08aa6d3a00b661bc24eff0c61
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-12 13:49:12 +0100

    feat: implemented StatusEffectSlot.cs

---
+++ Assets/Scripts/Status/StatusEffectSlot.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatusEffectSlot : MonoBehaviour
+{
+    [SerializeField] private Image m_Icon;
+    [SerializeField] private TMP_Text m_Duration;
+
+    private StatusEffect m_Effect;
+
+    public StatusEffectType Type => m_Effect.Type;
+
+    static Color ColorFor(StatusEffectType type) => type switch
+    {
+        StatusEffectType.Stunned => new Color(1.00f, 0.90f, 0.20f),
+        StatusEffectType.Vulnerable => new Color(1.00f, 0.55f, 0.15f),
+        StatusEffectType.Weak => new Color(0.55f, 0.55f, 0.55f),
+        StatusEffectType.Empowered => new Color(0.90f, 0.70f, 0.20f),
+        _ => Color.white  
+    };
+
+    void Refresh() => m_Duration.text = m_Effect.Duration.ToString();
+
+    public void Bind(StatusEffect effect)
+    {
+        m_Effect = effect;
+        m_Icon.color = ColorFor(effect.Type);
+
+        Refresh();
+    }
+}
