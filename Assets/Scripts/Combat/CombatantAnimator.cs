b8d3f22bc685985248c2f64e93fc338c144f92cf
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-14 21:44:48 +0100

    feat: add basic skeleton of CombatantAnimator.cs

---
+++ Assets/Scripts/Combat/CombatantAnimator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class CombatantAnimator : MonoBehaviour
+{
+    IEnumerator PlayWalkAnimation()
+    {
+        yield return null;
+    }
+
+    IEnumerator PlayAttackAnimation()
+    {
+        yield return null;
+    }
+
+    IEnumerator PlayHurtAnimation()
+    {
+        yield return null;
+    }
+
+    IEnumerator PlayDeathAnimation()
+    {
+        yield return null;
+    }
+}
