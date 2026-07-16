2376d29cb7a918042a94e5ed8dc026fabb8e1559
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-16 13:02:23 +0100

    feat: implemented basic skeleton of EnemyController.cs

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -2,14 +2,34 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // References
+    private BoardManager m_Board;
+    private EnemyObject m_Combatant;
+    private CombatantAnimator m_CombatantAnimator;
+
+    // State
+    private Vector2Int m_CellPosition;
+
+    public EnemyObject Combatant => m_Combatant;
+    public Vector2Int Cell => m_CellPosition;
+    
+    void Awake()
     {
-        
+        m_Combatant = GetComponent<EnemyObject>();
+        m_CombatantAnimator = GetComponent<CombatantAnimator>();    
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
+    {
+        GameManager.Instance.TurnManager.OnTick += OnTurnHappened;
+    }
+
+    void OnDestroy()
+    {
+        GameManager.Instance.TurnManager.OnTick += OnTurnHappened;
+    }
+
+    void OnTurnHappened()
     {
         
     }
