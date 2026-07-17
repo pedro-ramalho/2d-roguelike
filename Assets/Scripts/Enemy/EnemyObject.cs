c91e7fe497b11e9698c71fd11f1b1c12b18d94ed
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-17 13:15:35 +0100

    refactor: remove dead setup from Enemy prefab

--- Assets/Scripts/Enemy/EnemyObject.cs
+++ Assets/Scripts/Enemy/EnemyObject.cs
@@ -34,9 +34,6 @@ public class EnemyObject : CellObject, ICombatant
         GameManager.Instance.TurnManager.OnTick += TickStatusEffects;
 
         GetComponent<CombatantAnimator>().Bind(this);
-        GetComponentInChildren<CombatantBarsHUD>().Bind(this);
-        GetComponentInChildren<CombatantStatusEffectsHUD>().Bind(this);
-        GetComponentInChildren<CombatantFloatersHUD>().Bind(this);
     }
 
     void OnDestroy() => GameManager.Instance.TurnManager.OnTick -= TickStatusEffects;
