12de9b88ddf893ae48913a581b7181ab2456ccbb
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-22 20:36:58 +0100

    feat: blank implementation of Skirmisher.cs

--- Assets/Scripts/Enemy/Skirmisher.cs
+++ Assets/Scripts/Enemy/Skirmisher.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
 
-public class Skirmisher : MonoBehaviour
+public class Skirmisher : EnemyController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void ResolveEnemyAction()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        throw new System.NotImplementedException();
     }
 }
