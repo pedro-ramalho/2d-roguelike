512e3acd868ef83aacf510bf11437085febd865e
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-22 21:03:41 +0100

    feat: perfected Skirmisher.cs implementation

--- Assets/Scripts/Enemy/Skirmisher.cs
+++ Assets/Scripts/Enemy/Skirmisher.cs
@@ -1,20 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class Skirmisher : EnemyController
 {
-    protected override void ResolveEnemyAction()
+    private static readonly int m_SkirmisherTurns = 2;
+
+    IEnumerator ResolveSkirmisherAction()
     {
-        for (int attempt = 0; attempt < 2; attempt++)
+        for (int i = 0; i < m_SkirmisherTurns; i++)
         {
-            Vector2Int delta = ComputeDeltaToPlayer();
-            
+            Vector2Int delta = ComputeDeltaToPlayer();    
             if (IsAdjacentToPlayer(delta))
             {
                 AttackPlayer(delta);
+
                 break;
             }
             else
+            {
                 MoveTowards(delta);
+
+                yield return new WaitUntil(() => !Animator.IsBusy);
+            }
         }
+
     }
+
+    protected override void ResolveEnemyAction() => StartCoroutine(ResolveSkirmisherAction());
 }
