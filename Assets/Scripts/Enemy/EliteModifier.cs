dd7eb923c0297e8fd7213105ed820403a20551f3
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-26 09:08:47 +0100

    feat: implemented EliteModifier.cs, add OnEliteDefeated callback

--- Assets/Scripts/Enemy/EliteModifier.cs
+++ Assets/Scripts/Enemy/EliteModifier.cs
@@ -1,10 +1,29 @@
+using System;
 using UnityEngine;
 
 public class EliteModifier : MonoBehaviour
 {
+    private Combatant m_Combatant;
+
     void Awake()
     {
-        Combatant combatant = GetComponent<Combatant>();
-        combatant.ApplyStatMultiplier(2f);
+        m_Combatant = GetComponent<Combatant>();
+        m_Combatant.ApplyStatMultiplier(2f);
+
+        // Update the tint
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        sprite.color = new Color(138f/255f, 43f/255f, 226f/255f);
+
+        m_Combatant.Defeated += OnEliteDefeated;
+    }
+
+    void OnDestroy() => m_Combatant.Defeated -= OnEliteDefeated;
+
+    void OnEliteDefeated()
+    {
+        PlayerStat[] stats = (PlayerStat[])Enum.GetValues(typeof(PlayerStat));
+    
+        PlayerStat chosen = stats[UnityEngine.Random.Range(0, stats.Length)];
+        GameManager.Instance.PlayerController.UpgradeStat(chosen, 5);
     }
 }
