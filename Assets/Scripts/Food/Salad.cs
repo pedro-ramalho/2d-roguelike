c41c68f5ff4d8bd60c2a53ce688482cb21b5f934
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-29 12:08:13 +0100

    feat: implement Salad.cs

--- Assets/Scripts/Food/Salad.cs
+++ Assets/Scripts/Food/Salad.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class Salad : FoodObject
 {
-    protected override void ApplyEffect(PlayerController player)
-    {
-        throw new System.NotImplementedException();
-    }
+    [SerializeField] private int m_HealthPoints = 3;
+
+    protected override void ApplyEffect(PlayerController player) => player.Combatant.Heal(m_HealthPoints);
 }
