5a00077a155fa86df00856f106ca810f4fb81c7b
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-29 12:05:26 +0100

    feat: implement Hamburger effect

--- Assets/Scripts/Food/Hamburger.cs
+++ Assets/Scripts/Food/Hamburger.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class Hamburger : FoodObject
 {
-    protected override void ApplyEffect(PlayerController player)
-    {
-        throw new System.NotImplementedException();
-    }
+    [SerializeField] private int m_StaminaPoints = 5; 
+
+    protected override void ApplyEffect(PlayerController player) => player.PlayerStats.ChangeStamina(m_StaminaPoints);
 }
