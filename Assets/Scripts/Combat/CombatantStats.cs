7dd3e04f080b32c1a0c0e553ac37aa2bbb0190e5
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-07-31 10:24:12 +0100

    feat: populated CombatantStats.cs with relevant stats

--- Assets/Scripts/Combat/CombatantStats.cs
+++ Assets/Scripts/Combat/CombatantStats.cs
@@ -1,4 +1,17 @@
 public class CombatantStats 
 {
-    
+    // Health
+    public int MaxHP;
+    public int HP;
+
+    // Block
+    public int MaxBlock;
+    public int Block;
+
+    // Attack
+    public int Attack;
+
+    // Stamina (unused by enemies)
+    public int MaxStamina;
+    public int Stamina;
 }
