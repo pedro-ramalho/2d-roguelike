7e8a501c8f749b365ad7873f3251b28b1e03cd8a
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-08-01 17:38:28 +0100

    feat: add LevelProgressionSettings.cs

---
+++ Assets/Scripts/Level/LevelProgressionSettings.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class LevelBand
+{
+    public string Name;
+    public int MinLevel;
+    public int MaxLevel;
+    public LevelConfig DefaultConfig;
+}
+
+[CreateAssetMenu(
+    fileName = "LevelProgressionSettings",
+    menuName = "Game/Level Progression Settings"
+)]
+public class LevelProgressionSettings : ScriptableObject
+{
+    [Header("Elite Cadence")]
+    public int EliteCadence = 3;
+
+    [Header("Status Rolls")]
+    [Range(0f, 1f)]
+    public float MaxStatusChance;
+
+    [Header("Level Bands")]
+    public LevelBand[] LevelBands;
+}
