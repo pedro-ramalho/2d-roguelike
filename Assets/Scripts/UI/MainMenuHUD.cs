5b3f5dd8d645652a735cf6e678de6067bc6cc095
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-08-05 17:01:08 +0100

    feat: implemented skeleton of MainMenuHUD.cs

--- Assets/Scripts/UI/MainMenuHUD.cs
+++ Assets/Scripts/UI/MainMenuHUD.cs
@@ -6,9 +6,30 @@ public class MainMenuHUD : MonoBehaviour
     [SerializeField]
     private UIDocument m_UIDocument;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    private Button m_StartRunButton;
+    private Button m_SettingsButton;
+    private Button m_CreditsButton;
+    private Button m_QuitButton;
 
-    // Update is called once per frame
-    void Update() { }
+    void Start()
+    {
+        VisualElement root = m_UIDocument.rootVisualElement;
+        VisualElement buttonContainer = root.Q<VisualElement>("ButtonContainer");
+
+        m_StartRunButton = buttonContainer.Q<Button>("StartButton");
+        m_SettingsButton = buttonContainer.Q<Button>("SettingsButton");
+        m_CreditsButton = buttonContainer.Q<Button>("CreditsButton");
+        m_QuitButton = buttonContainer.Q<Button>("QuitButton");
+    }
+
+    void OnStartRunButtonPress() { }
+
+    void OnSettingsButtonPress() { }
+
+    void OnCreditsButtonPress() { }
+
+    void OnQuitButtonPress()
+    {
+        Application.Quit();
+    }
 }
