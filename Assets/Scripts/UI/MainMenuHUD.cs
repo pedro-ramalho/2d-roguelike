1e8b6aa2803da80f726f8abe4f69b730ea6ecfad
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-08-05 17:35:07 +0100

    feat: finish initial version of MainMenu.uxml and wiring through MainMenuHUD.cs

--- Assets/Scripts/UI/MainMenuHUD.cs
+++ Assets/Scripts/UI/MainMenuHUD.cs
@@ -5,30 +5,57 @@ using UnityEngine.UIElements;
 using UnityEditor;
 #endif
 
+enum PanelType
+{
+    Buttons,
+    Settings,
+    Credits,
+}
+
 public class MainMenuHUD : MonoBehaviour
 {
     [SerializeField]
     private UIDocument m_UIDocument;
 
+    // Buttons Panel
+    private VisualElement m_ButtonsPanel;
     private Button m_StartRunButton;
     private Button m_SettingsButton;
     private Button m_CreditsButton;
     private Button m_QuitButton;
 
+    // Credits Panel
+    private VisualElement m_CreditsPanel;
+    private Button m_CreditsBackButton;
+
+    // Settings Panel
+    private VisualElement m_SettingsPanel;
+    private Button m_SettingsBackButton;
+
     void Start()
     {
         VisualElement root = m_UIDocument.rootVisualElement;
-        VisualElement buttonContainer = root.Q<VisualElement>("ButtonContainer");
+        VisualElement mainMenuPanel = root.Q<VisualElement>("MainMenuPanel");
 
-        m_StartRunButton = buttonContainer.Q<Button>("StartButton");
-        m_SettingsButton = buttonContainer.Q<Button>("SettingsButton");
-        m_CreditsButton = buttonContainer.Q<Button>("CreditsButton");
-        m_QuitButton = buttonContainer.Q<Button>("QuitButton");
+        m_ButtonsPanel = mainMenuPanel.Q<VisualElement>("ButtonsPanel");
+        m_CreditsPanel = mainMenuPanel.Q<VisualElement>("CreditsPanel");
+        m_SettingsPanel = mainMenuPanel.Q<VisualElement>("SettingsPanel");
+
+        m_StartRunButton = m_ButtonsPanel.Q<Button>("StartRunButton");
+        m_CreditsButton = m_ButtonsPanel.Q<Button>("CreditsButton");
+        m_SettingsButton = m_ButtonsPanel.Q<Button>("SettingsButton");
+        m_QuitButton = m_ButtonsPanel.Q<Button>("QuitButton");
+
+        m_CreditsBackButton = m_CreditsPanel.Q<Button>("CreditsBackButton");
+        m_SettingsBackButton = m_SettingsPanel.Q<Button>("SettingsBackButton");
 
         m_StartRunButton.clicked += OnStartRunButtonPress;
-        m_SettingsButton.clicked += OnSettingsButtonPress;
         m_CreditsButton.clicked += OnCreditsButtonPress;
+        m_SettingsButton.clicked += OnSettingsButtonPress;
         m_QuitButton.clicked += OnQuitButtonPress;
+
+        m_CreditsBackButton.clicked += OnCreditsBackButtonPress;
+        m_SettingsBackButton.clicked += OnSettingsBackButtonPress;
     }
 
     void OnDestroy()
@@ -44,6 +71,22 @@ public class MainMenuHUD : MonoBehaviour
 
         if (m_QuitButton != null)
             m_QuitButton.clicked -= OnQuitButtonPress;
+
+        if (m_CreditsBackButton != null)
+            m_CreditsBackButton.clicked -= OnCreditsBackButtonPress;
+
+        if (m_SettingsBackButton != null)
+            m_SettingsBackButton.clicked -= OnSettingsBackButtonPress;
+    }
+
+    void ShowPanel(PanelType panel)
+    {
+        m_ButtonsPanel.style.display =
+            panel == PanelType.Buttons ? DisplayStyle.Flex : DisplayStyle.None;
+        m_SettingsPanel.style.display =
+            panel == PanelType.Settings ? DisplayStyle.Flex : DisplayStyle.None;
+        m_CreditsPanel.style.display =
+            panel == PanelType.Credits ? DisplayStyle.Flex : DisplayStyle.None;
     }
 
     void OnStartRunButtonPress()
@@ -51,11 +94,13 @@ public class MainMenuHUD : MonoBehaviour
         SceneManager.LoadScene("Main");
     }
 
-    void OnSettingsButtonPress() { /* TBD */
-    }
+    void OnCreditsButtonPress() => ShowPanel(PanelType.Credits);
 
-    void OnCreditsButtonPress() { /* TBD */
-    }
+    void OnCreditsBackButtonPress() => ShowPanel(PanelType.Buttons);
+
+    void OnSettingsButtonPress() => ShowPanel(PanelType.Settings);
+
+    void OnSettingsBackButtonPress() => ShowPanel(PanelType.Buttons);
 
     void OnQuitButtonPress()
     {
