abf9e441ff7b2fba3555a949a8207058710ae633
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-08-05 16:56:42 +0100

    feat: add empty MainMenuHUD.cs

---
+++ Assets/Scripts/UI/MainMenuHUD.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuHUD : MonoBehaviour
+{
+    [SerializeField]
+    private UIDocument m_UIDocument;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
