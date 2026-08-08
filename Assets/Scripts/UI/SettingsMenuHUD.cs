8d2fae4b294cd1dc0311f167474bff7b5f7ff689
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-08-08 21:14:54 +0100

    feat: implemented SettingsMenuHUD.cs

--- Assets/Scripts/UI/SettingsMenuHUD.cs
+++ Assets/Scripts/UI/SettingsMenuHUD.cs
@@ -1,10 +1,57 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SettingsMenuHUD : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    [SerializeField]
+    private UIDocument m_UIDocument;
 
-    // Update is called once per frame
-    void Update() { }
+    private VisualElement m_SettingsMenuPanel;
+    private Slider m_MasterVolumeSlider;
+    private Slider m_MusicVolumeSlider;
+    private Slider m_SFXVolumeSlider;
+    private Button m_BackButton;
+
+    void Awake()
+    {
+        VisualElement root = m_UIDocument.rootVisualElement;
+
+        m_SettingsMenuPanel = root.Q<VisualElement>("SettingsMenuPanel");
+        m_MasterVolumeSlider = m_SettingsMenuPanel.Q<Slider>("MasterVolumeSlider");
+        m_MusicVolumeSlider = m_SettingsMenuPanel.Q<Slider>("MusicVolumeSlider");
+        m_SFXVolumeSlider = m_SettingsMenuPanel.Q<Slider>("SFXVolumeSlider");
+        m_BackButton = m_SettingsMenuPanel.Q<Button>("BackButton");
+
+        m_MasterVolumeSlider.RegisterValueChangedCallback(OnMasterVolumeChanged);
+        m_MusicVolumeSlider.RegisterValueChangedCallback(OnMusicVolumeChanged);
+        m_SFXVolumeSlider.RegisterValueChangedCallback(OnSFXVolumeChanged);
+
+        m_BackButton.clicked += OnBackButtonPress;
+    }
+
+    void OnDestroy()
+    {
+        if (m_MasterVolumeSlider != null)
+            m_MasterVolumeSlider.UnregisterValueChangedCallback(OnMasterVolumeChanged);
+
+        if (m_MusicVolumeSlider != null)
+            m_MusicVolumeSlider.UnregisterValueChangedCallback(OnMusicVolumeChanged);
+
+        if (m_SFXVolumeSlider != null)
+            m_SFXVolumeSlider.UnregisterValueChangedCallback(OnSFXVolumeChanged);
+
+        if (m_BackButton != null)
+            m_BackButton.clicked -= OnBackButtonPress;
+    }
+
+    void OnMasterVolumeChanged(ChangeEvent<float> evt) =>
+        AudioManager.Instance.SetVolume("MasterVolume", evt.newValue);
+
+    void OnMusicVolumeChanged(ChangeEvent<float> evt) =>
+        AudioManager.Instance.SetVolume("MusicVolume", evt.newValue);
+
+    void OnSFXVolumeChanged(ChangeEvent<float> evt) =>
+        AudioManager.Instance.SetVolume("SFXVolume", evt.newValue);
+
+    void OnBackButtonPress() => m_SettingsMenuPanel.style.display = DisplayStyle.None;
 }
