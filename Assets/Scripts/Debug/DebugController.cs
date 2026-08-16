179a8d22f07af863d313f96359d4d4ded208d2af
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-08-16 14:01:11 +0100

    feat: started working on the DebugController.cs

--- Assets/Scripts/Debug/DebugController.cs
+++ Assets/Scripts/Debug/DebugController.cs
@@ -1,10 +1,52 @@
+#if UNITY_EDITOR
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DebugController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    [SerializeReference]
+    private Combatant m_Player;
 
-    // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        if (Keyboard.current.vKey.wasPressedThisFrame)
+            m_Player.ApplyStatusEffect(new StatusEffectEmpowered(2));
+
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+            m_Player.ApplyStatusEffect(new StatusEffectVulnerable(3));
+
+        if (Keyboard.current.nKey.wasPressedThisFrame)
+            m_Player.ApplyStatusEffect(new StatusEffectWeak(4));
+
+        if (Keyboard.current.mKey.wasPressedThisFrame)
+            m_Player.ApplyStatusEffect(new StatusEffectStunned(1));
+
+        if (Keyboard.current.oKey.wasPressedThisFrame)
+            m_Player.TakeDamage(1);
+
+        if (Keyboard.current.pKey.wasPressedThisFrame)
+            m_Player.Heal(1);
+
+        if (Keyboard.current.gKey.wasPressedThisFrame)
+        {
+            // Jump forward one level
+        }
+
+        if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            // Skip to next band
+        }
+
+        if (Keyboard.current.jKey.wasPressedThisFrame)
+        {
+            // Toggle god mode
+        }
+
+        if (Keyboard.current.lKey.wasPressedThisFrame)
+        {
+            // Reload current level
+        }
+    }
 }
+
+#endif
