0dc4f07427517f2312c41f5ec161db5b2d35de7e
Pedro Ramalho <72579386+pedro-ramalho@users.noreply.github.com>
2026-09-11 17:20:51 +0100

    feat: setup unit test environment and add basic smoke test

---
+++ Assets/Tests/SmokeTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SmokeTest
+    {
+        [Test]
+        public void TrueEqualsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
